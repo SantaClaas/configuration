eaf8e60dff2a11f3e57a7c9ec3272969b63c4ef8
Claas <claas@claas.dev>
2024-09-18 11:58:07 +0200

    Try to fix script and add hint
    I don't know bash

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,10 @@
 # Bootstrapping nix and configuring nix to the point it can configure the rest
-
+# `chmod u+x` after git clone (change mode) to make file executable (+x) for current user (u)
 echo "Copying nix.conf to ~/.config/nix/"
 cp ./nix.conf ~/.config/nix/
 
 echo "Running nix install script"
-sh <(curl -L https://nixos.org/nix/install)
+sh < (curl -L "https://nixos.org/nix/install")
 
 echo "Installing nix-darwin"
 nix run nix-darwin -- switch --flake ~/.config/nix-darwin
