03e803fb99578b48da243fbca7fe12d2796b3d73
Claas <claas@claas.dev>
2024-09-18 11:43:53 +0200

    Add script to test on VM

--- initialize.sh
+++ initialize.sh
@@ -1,4 +1,10 @@
-echo "TODO set up nix"
+# Bootstrapping nix and configuring nix to the point it can configure the rest
 
 echo "Copying nix.conf to ~/.config/nix/"
 cp ./nix.conf ~/.config/nix/
+
+echo "Running nix install script"
+sh <(curl -L https://nixos.org/nix/install)
+
+echo "Installing nix-darwin"
+nix run nix-darwin -- switch --flake ~/.config/nix-darwin
